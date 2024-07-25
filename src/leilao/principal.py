8d1bc52ec3607b31691193b039177432a51a3630
afonso coelho <afonsogcoelho@gmail.com>
2024-07-25 16:35:34 -0400

    organizando tudo para começar a testar

--- src/leilao/principal.py
+++ src/leilao/principal.py
@@ -1,10 +1,10 @@
-from dominio import Usuario, Lance, Leilao
+from dominio import Avaliador, Usuario, Lance, Leilao
 
 afonso = Usuario('Afonso')
 alice = Usuario('Alice')
 
-lance_da_alice = Lance(alice, 200)
-lance_do_afonso = Lance(afonso, 250)
+lance_da_alice = Lance(alice, 250)
+lance_do_afonso = Lance(afonso, 200)
 
 
 leilao = Leilao('celular')
@@ -14,3 +14,9 @@ leilao.lances.append(lance_do_afonso)
 
 for lance in leilao.lances:
     print(f'O usuario {lance.usuario.nome} deu um lance de {lance.valor}')
+
+avaliador = Avaliador()
+avaliador.avalia(leilao)
+
+print(f'O menor lance foi de {avaliador.menor_lance} e o maior foi de {avaliador.maior_lance}')
+
