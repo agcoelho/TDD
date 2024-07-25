2e9660da09ed4b694db8902dd1410d441cb4f235
afonso coelho <afonsogcoelho@gmail.com>
2024-07-25 18:09:09 -0400

    adaptando a classe avaliador para uma def de leilao

--- src/leilao/test_unittests.py
+++ src/leilao/test_unittests.py
@@ -4,7 +4,7 @@ import os
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'src')))
 
 
-from dominio import Avaliador, Lance, Leilao, Usuario
+from dominio import Lance, Leilao, Usuario
 
 
 class TestAvaliador(TestCase):
@@ -22,44 +22,41 @@ class TestAvaliador(TestCase):
         self.leilao = Leilao('celular')
 
     def test_avalia(self):
-
+        test.setUp()
 
 
         self.leilao.propoe(self.lance_da_alice)
         self.leilao.propoe(self.lance_do_afonso)
 
 
-        avaliador = Avaliador()
-        avaliador.avalia(self.leilao)
-        print(f'O menor lance foi de {avaliador.menor_lance} e o maior foi de {avaliador.maior_lance}')
+        print(f'O menor lance foi de {self.leilao.menor_lance} e o maior foi de {self.leilao.maior_lance}')
 
         menor_valor_esperado = 200.0
         maior_valor_esperado = 250.0
 
-        self.assertEqual(menor_valor_esperado, avaliador.menor_lance)
-        self.assertEqual(maior_valor_esperado, avaliador.maior_lance)
+        self.assertEqual(menor_valor_esperado, self.leilao.menor_lance)
+        self.assertEqual(maior_valor_esperado, self.leilao.maior_lance)
 
 
 
     def test_avalia2(self):
+        test.setUp()
 
         self.leilao.propoe(self.lance_do_afonso)
         self.leilao.propoe(self.lance_da_alice)
       
 
-
-        avaliador = Avaliador()
-        avaliador.avalia(self.leilao)
-        print(f'O menor lance foi de {avaliador.menor_lance} e o maior foi de {avaliador.maior_lance}')
+        print(f'O menor lance foi de {self.leilao.menor_lance} e o maior foi de {self.leilao.maior_lance}')
 
         menor_valor_esperado = 200.0
         maior_valor_esperado = 250.0
 
-        self.assertEqual(menor_valor_esperado, avaliador.menor_lance)
-        self.assertEqual(maior_valor_esperado, avaliador.maior_lance)
+        self.assertEqual(menor_valor_esperado, self.leilao.menor_lance)
+        self.assertEqual(maior_valor_esperado, self.leilao.maior_lance)
 
 
     def test_deve_retornar_o_mesmo_valor_para_maior_e_menor_lance_quando_o_leilao_tiver_um_lance(self):
+
         joao = Usuario('joão')
 
         lance = Lance(joao, 200)
@@ -68,29 +65,30 @@ class TestAvaliador(TestCase):
         leilao = Leilao('celular')
         leilao.propoe(lance)
 
-        avaliador = Avaliador()
-        avaliador.avalia(leilao)
 
-        self.assertEqual(200.0, avaliador.menor_lance)
-        self.assertEqual(200.0, avaliador.maior_lance)
+        self.assertEqual(200.0, leilao.menor_lance)
+        self.assertEqual(200.0, leilao.maior_lance)
+
+        print(f'O menor lance foi de {leilao.menor_lance} e o maior foi de {leilao.maior_lance}')
+
+
 
     def test_deve_retornar_o_maior_e_o_menor_valor_quando_o_leilao_tiver_tres_lances(self):
+        test.setUp()
+
 
         self.leilao.propoe(self.lance_do_afonso)
         self.leilao.propoe(self.lance_da_alice)
         self.leilao.propoe(self.lance_da_alita)
       
-
-
-        avaliador = Avaliador()
-        avaliador.avalia(self.leilao)
-        print(f'O menor lance foi de {avaliador.menor_lance} e o maior foi de {avaliador.maior_lance}')
+      
+        print(f'O menor lance foi de {self.leilao.menor_lance} e o maior foi de {self.leilao.maior_lance}')
 
         menor_valor_esperado = 200.0
         maior_valor_esperado = 500.0
 
-        self.assertEqual(menor_valor_esperado, avaliador.menor_lance)
-        self.assertEqual(maior_valor_esperado, avaliador.maior_lance)
+        self.assertEqual(menor_valor_esperado, self.leilao.menor_lance)
+        self.assertEqual(maior_valor_esperado, self.leilao.maior_lance)
 
         
 
@@ -99,7 +97,7 @@ class TestAvaliador(TestCase):
 
 test = TestAvaliador()
 
-test.setUp()
+
 
 test.test_avalia()
 
