fcc99df3921771514727676e42de288246608351
afonso coelho <afonsogcoelho@gmail.com>
2024-07-25 17:17:46 -0400

    corrigindo valores dos testes

--- src/leilao/test_unittests.py
+++ src/leilao/test_unittests.py
@@ -56,8 +56,8 @@ class TestAvaliador(TestCase):
         avaliador.avalia(leilao)
         print(f'O menor lance foi de {avaliador.menor_lance} e o maior foi de {avaliador.maior_lance}')
 
-        menor_valor_esperado = 100.0
-        maior_valor_esperado = 100.0
+        menor_valor_esperado = 200.0
+        maior_valor_esperado = 250.0
 
         self.assertEqual(menor_valor_esperado, avaliador.menor_lance)
         self.assertEqual(maior_valor_esperado, avaliador.maior_lance)
@@ -75,8 +75,41 @@ class TestAvaliador(TestCase):
         avaliador = Avaliador()
         avaliador.avalia(leilao)
 
-        self.assertEqual(150.0, avaliador.menor_lance)
-        self.assertEqual(150.0, avaliador.maior_lance)
+        self.assertEqual(200.0, avaliador.menor_lance)
+        self.assertEqual(200.0, avaliador.maior_lance)
+
+    def test_deve_retornar_o_maior_e_o_menor_valor_quando_o_leilao_tiver_tres_lances(self):
+
+        afonso = Usuario('Afonso')
+        alice = Usuario('Alice')
+        alita = Usuario('Alita')
+
+
+        lance_da_alice = Lance(alice, 250)
+        lance_do_afonso = Lance(afonso, 200)
+        lance_da_alita = Lance(alita, 500)
+
+
+        leilao = Leilao('celular')
+
+        leilao.lances.append(lance_do_afonso)
+        leilao.lances.append(lance_da_alice)
+        leilao.lances.append(lance_da_alita)
+      
+
+
+        avaliador = Avaliador()
+        avaliador.avalia(leilao)
+        print(f'O menor lance foi de {avaliador.menor_lance} e o maior foi de {avaliador.maior_lance}')
+
+        menor_valor_esperado = 200.0
+        maior_valor_esperado = 500.0
+
+        self.assertEqual(menor_valor_esperado, avaliador.menor_lance)
+        self.assertEqual(maior_valor_esperado, avaliador.maior_lance)
+
+        
+
 
 
 
